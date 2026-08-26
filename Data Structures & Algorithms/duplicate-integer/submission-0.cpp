5ef794919b2306874b91b852bbb1b0fd4a1b68f1
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-26 21:09:30 +0100

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> hashSet ;
+        for (auto num : nums) {
+            if (hashSet.find(num) != hashSet.end()){
+                return true;
+            }
+            hashSet.insert(num);
+        }
+        return false;
+        
+    }
+};
