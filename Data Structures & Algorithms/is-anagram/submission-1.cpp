c8e6b425600404f88d70770e37b1b309a7af2441
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-27 16:36:59 +0100

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+
+        int arr[26] = {0};
+
+        for (int i { 0 }; i < s.length(); i ++){
+            arr[s[i] - 'a']++;
+        }
+
+        for (int i { 0 }; i < t.length(); i++){
+            arr[t[i] - 'a']--;
+        }
+
+        for (int alph : arr){
+            if (alph != 0) return false;
+        }
+        return true;
+        
+    }
+};
